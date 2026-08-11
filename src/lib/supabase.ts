1fc946de3bc65b71a17ec03b05185ae2ed4dc3fb
anime1234rr <arandaa196@gmail.com>
2026-08-11 20:18:28 +0200

    variables corregidas

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = (process.env.EXPO_PUBLIC_SUPABASE_URL as string).trim()
+const supabaseAnonKey = (process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string).trim()
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
