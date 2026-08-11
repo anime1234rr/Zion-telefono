13c56fe97f400868d22eb0080bf0448dbc9090b1
anime1234rr <arandaa196@gmail.com>
2026-08-11 23:57:54 +0200

    posicion de texto n7

--- src/screens/servers/ChannelChatScreen.tsx
+++ src/screens/servers/ChannelChatScreen.tsx
@@ -217,7 +217,7 @@ export function ChannelChatScreen() {
       <KeyboardAvoidingView
         style={styles.flex}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 15}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 19}
       >
         <MessageList
           messages={messages}
